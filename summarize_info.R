24276cef9903e84254b8d823bce1c2b4d54dc0bc
annazhou <you-email-address>
2019-05-20 18:18:09 -0700

    added ny dataset

--- summarize_info.R
+++ summarize_info.R
@@ -1,1 +1,7 @@
-# Summarizing Data
+# This file summarizes the information of the file
+
+library(dplyr)
+library(tidyr)
+
+# load data
+collisions <- read.csv("data/mvc_threeyear_ny.csv", stringsAsFactors = FALSE)
