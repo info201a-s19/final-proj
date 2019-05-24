1f6c558900a1797983e3f595016fd190e3534363
annazhou <you-email-address>
2019-05-24 02:50:46 -0700

    fixed style

--- scripts/make_table.R
+++ scripts/make_table.R
@@ -3,16 +3,15 @@ library(dplyr)
 table_maker <- function(df) {
         df <- df %>%
             mutate(
-                pct_BAC_over_0 = Driver.BAC....01...07..Percent + 
+                pct_BAC_over_0 = Driver.BAC....01...07..Percent +
                     Driver.BAC....08...Percent
             )
-    
         stats <- df %>%
-            select(State, Total.Drivers.Involved.in.Fatal.Crashes..Number, 
+            select(State, Total.Drivers.Involved.in.Fatal.Crashes..Number,
                    pct_BAC_over_0) %>%
             group_by(Total.Drivers.Involved.in.Fatal.Crashes..Number) %>%
             arrange(-Total.Drivers.Involved.in.Fatal.Crashes..Number)
-        colnames(stats) <- c("State", 
+        colnames(stats) <- c("State",
                              "Number of Drivers Involved in Fatal Crashes",
                              "Percent of Drivers w/ BAC over 0.1")
         return(stats)
--- scripts/summarize_info.R
+++ scripts/summarize_info.R
@@ -5,17 +5,16 @@ library(dplyr)
 get_summary_info <- function(df) {
     ret <- list()
     alc_accidents <- filter(
-        df, 
+        df,
         Contributing.Factor.1.Description == "Alcohol Involvement"
     )
     ret$total_crashes <- nrow(df)
     ret$total_alc_crashes <- nrow(alc_accidents)
-    ret$pct_crash <- round((ret$total_alc_crashes / ret$total_crashes) * 100.0, 
+    ret$pct_crash <- round( (ret$total_alc_crashes / ret$total_crashes) * 100.0,
                            digits = 2)
     accident_year <- df %>%
         group_by(Year) %>%
         tally()
-    
     ret$avg <- mean(accident_year$n)
     car_type <- df %>%
         group_by(Vehicle.Body.Type) %>%
@@ -23,6 +22,5 @@ get_summary_info <- function(df) {
     ret$most_common_car <- car_type %>%
         filter(n == max(n)) %>%
         pull(Vehicle.Body.Type)
-    
     return(ret)
 }
