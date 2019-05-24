7ccac498ecc83b159cbbfd97be01a41beb7acf1c
annazhou <you-email-address>
2019-05-24 02:41:17 -0700

    finished summary info and table

--- scripts/make_table.R
+++ scripts/make_table.R
@@ -1,18 +1,24 @@
 # make table
 
 library(dplyr)
-library(ggplot2)
-library(leaflet)
 source("summarize_info.R")
 
-accidents <- collisions
+fatal_crash <- read.csv("~/INFO/201/final-proj/data/dataset1.csv", stringsAsFactors = FALSE)
 
-ny <- get_summary_info(accidents)
-
-ny <- data.frame(ny)
 
 table_maker <- function(df) {
-    ret <- get_summary_info(df)
-    ret <- data.frame(ny)
+        df <- df %>%
+            mutate(
+                pct_BAC_over_0 = Driver.BAC....01...07..Percent + Driver.BAC....08...Percent
+            )
     
+        stats <- df %>%
+            select(State, Total.Drivers.Involved.in.Fatal.Crashes..Number, pct_BAC_over_0) %>%
+            group_by(Total.Drivers.Involved.in.Fatal.Crashes..Number) %>%
+            arrange(-Total.Drivers.Involved.in.Fatal.Crashes..Number)
+        colnames(stats) <- c("State", "Number of Drivers Involved in Fatal Crahses", 
+                             "Percent of Drivers w/ BAC over 0.1")
+        return(stats)
 }
+
+yeet <- table_maker(fatal_crash)
