9bc973e87c5e6836afc33bd9703a5515adfee7d9
annazhou <you-email-address>
2019-05-24 00:59:34 -0700

    small fixes

--- scripts/make_table.R
+++ scripts/make_table.R
@@ -1,1 +1,18 @@
 # make table
+
+library(dplyr)
+library(ggplot2)
+library(leaflet)
+source("summarize_info.R")
+
+accidents <- collisions
+
+ny <- get_summary_info(accidents)
+
+ny <- data.frame(ny)
+
+table_maker <- function(df) {
+    ret <- get_summary_info(df)
+    ret <- data.frame(ny)
+    
+}
