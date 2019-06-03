369c589ae5492ad037a3e809294dbea99b6338ef
Lisa Marie Dias <diaslisamarie@gmail.com>
2019-06-03 14:12:24 -0700

    final edit

---
+++ SummaryTakeaways.R
@@ -0,0 +1,25 @@
+3 Major Takeaways
+
+"1)
+As can be seen from the data presented in various states in the US, having a BAC 
+of over 0.8, which also characterizes one as having a DUI is strongly correlated 
+with car accident injuries and fatalities. From real life examples from the 
+states of ___ and ____specifically, one can see 
+that the largest percentage of car accidents occur from those that are either 
+unrelated to alcohol, or ones that have a BAC level of over 0.8. This is 
+important to raise awareness for because of how incredibly easy it is to reach 
+this BAC level without one even realizing it. Thus, people should try to avoid 
+drinking and driving at all costs if possible. 
+
+2) 
+One of the ways to examine whether a driver with higher blood 
+alcohol concentration (BAC) more likely to be involved in a crash is by 
+comparing the percentage of drivers with 0.01-0.07 BAC versus those with 0.08+ 
+BAC. By looking at the percentage values, we see a general trend that the 
+percentage of drivers who were over the limit in fatal crashes is always greater 
+than the percentage of those intoxicated but under the limit
+
+
+3)
+
+""
